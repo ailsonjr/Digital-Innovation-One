bc75f12a05f623a2ff76bf3a4b77389e8bab1456
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-11 17:42:03 -0300

    Adicionado botão Delete para deletar os cursos da lista de cursos

--- Angular/course-maneger/src/app/courses/course-list.component.ts
+++ Angular/course-maneger/src/app/courses/course-list.component.ts
@@ -28,6 +28,16 @@ export class CourseListComponent implements OnInit {
     })
   }
 
+  deleteById(courseId: number): void {
+    this.courseService.deleteById(courseId).subscribe({
+      next: () => {
+        console.log('Delete with sucess');
+        this.retrieveAll();
+      },
+      error: err => console.log('Error', err)
+    })
+  }
+
   set filter(value: string) {
     this._filterBy = value;
 
