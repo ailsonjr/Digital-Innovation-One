2a8e0abb7121303a3accdcb87b6dd9efb3d02948
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-17 15:31:08 -0300

    Criado componente Home e adicionado framework do Angula Material

--- Projetos/spaangular/src/app/app.module.ts
+++ Projetos/spaangular/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
