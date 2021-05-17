58aed49786f07d146cfda13707b1eb48907d37f8
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-17 00:35:17 -0300

    Realizado mudanças conforme orientado em aula

--- Angular/jv-state/src/app/app.component.spec.ts
+++ Angular/jv-state/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'jv-state'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jv-state');
+    expect(app).toEqual('jv-state');
   });
 
   it('should render title', () => {
